4d659b62231862fe7c34de36b332f6f12e471350
kppankow <kelvinpankow@yahoo.com>
2017-05-12 16:55:29 -0400

    4.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509173145) do
+ActiveRecord::Schema.define(version: 20170512205104) do
 
   create_table "object_names", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20170509173145) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "colour"
+    t.string   "price"
   end
 
 end
