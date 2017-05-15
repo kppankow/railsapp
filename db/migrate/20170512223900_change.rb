a0605939e614dbd56cf0000a582514353f4e6373
kppankow <kelvinpankow@yahoo.com>
2017-05-15 17:32:01 -0400

    4.4

--- db/migrate/20170512223900_change.rb
+++ db/migrate/20170512223900_change.rb
@@ -1,5 +1,5 @@
 class Change < ActiveRecord::Migration[5.0]
   def change
-  	change_column :products, :price, :integer
+  	change_column :products, :price, 'integer USING CAST(price AS integer)'
   end
 end
