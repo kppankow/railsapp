2d67ac1975b48b8cbbafa0cda867b2c99cff4569
kppankow <kelvinpankow@yahoo.com>
2017-05-28 10:18:03 -0400

    4.6 page change

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  get 'static_pages/landing_page'
+  root 'static_pages#landing_page'
 
-  root 'products#index'
+  get 'products/index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
