0d92184019ec22689512862551364bc8e7d8dba8
kppankow <kelvinpankow@yahoo.com>
2017-06-07 09:46:47 -0400

    5.4 user_mail additions

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'products/index'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   #  For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
