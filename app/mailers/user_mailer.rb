e94682959e5caf6b3affaa49402a169411787a60
kppankow <kelvinpankow@yahoo.com>
2017-06-20 10:13:06 -0400

    5.11 user_email sign up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-	default from: "from@example.com"
+	default from: "kelvinpankow@yahoo.com"
 
 		def contact_form(email, name, message)
 			@message = message
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
         			to: 'kelvinpankow@yahoo.com',
         			subject: "A new contact form message from #{name}")
   		end
+
+  		def welcome(user)
+  			@user = user
+  			@appname = "Berlin Bikes"
+  			mail(to: user.email,
+       			subject: "Welcome to #{@appname}!")
+		end
 end
