bd14586600af8f27dc0eb3b29d073a08cf6754fe
kppankow <kelvinpankow@yahoo.com>
2017-06-26 13:17:48 -0400

    6.3 rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
-describe UsersController, type: :controller do 
-	let(:user) { User.create!(email: 'fakemail@gmail.com', password: '123456') }
-	let(:user) { User.create!(email: 'fakemail2@gmail.com', password: '123456') }
-
-	
-
+describe UsersController, type: :controller do
+	before do
+		@user = User.create!(email: 'fakemail@gmail.com', password: '123456', first_name: 'steve', last_name: 'brown')
+		@user2 = User.create!(email: 'fakemail2@gmail.com', password: '123456', first_name: 'bob', last_name: 'smith')
+	end
 	describe 'GET #show' do
 		context 'when a user is logged in' do
 			before do
-				sign_in user
+				sign_in @user
 			end
 			it 'loads correct user details' do
-				get :show, params: { id: user.id}
-				expect(assigns(:user)).to eq user
-
+				get :show, params: { id: @user.id }
+				expect(assigns(:user)).to eq @user
+				expect(response).to have_http_status(200)
+			end
+			it 'cant access other users show page' do
+				get :show, params: { id: @user2.id}
+				expect(resonse).to redirect_to(root_path)
+			end
 		end
 
-		context 'when a user is not logged in' do
+		context 'User is not logged in' do
 			it 'redirects to login' do
-				get :show, params: { id: user.id }
+				get :show, params: { id: @user.id }
 				expect(response).to redirect_to(new_user_session_path)
 			end
 		end
+		
 	end
-
 end
