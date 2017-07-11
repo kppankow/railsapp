9f4cdeab983ab3a1724c51baa2c5d4d809ee538a
kppankow <kelvinpankow@yahoo.com>
2017-07-11 13:23:05 -0400

    6.12 view count

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,8 +13,17 @@ class Product < ApplicationRecord
 	def highest_rating_comment
 		comments.rating_desc.first
 	end
+	def lowest_rating_comment
+	  comments.rating_asc.first
+	end
 	def average_rating
 		comments.average(:rating).to_f
+	end
+	def views
+	  $redis.get("product:#{id}")
+	end
+	def viewed!
+	  $redis.incr("product:#{id}") 
 	end
 
 end
