81f540b716f4acc685fd0890f34927aa68fc9498
kppankow <kelvinpankow@yahoo.com>
2017-07-13 18:29:48 -0400

    6.13

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.web_socket_server_url = "wss://berlin-bikes.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://berlin-bikes.herokuapp.com', 'http://berlin-bikes.herokuapp.com']
 end
